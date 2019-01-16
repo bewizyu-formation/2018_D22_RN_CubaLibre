d75dcede9ba671a7059687e3b5c4432eac108c2a
Kevyn <kevyn.buchet@gmail.com>
2019-01-16 15:04:59 +0100

    Finition de l'écran de détail

--- src/components/ContactDetailContainer.js
+++ src/components/ContactDetailContainer.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  ScrollView, View, Text, Image, StyleSheet,
+  ScrollView, View, Text, Image, StyleSheet, Alert, TouchableOpacity,
 } from 'react-native';
 
 const grey = '#F5FCFF';
-const black = '#000';
+const white = '#fff';
+const purple = '#841584';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: grey,
     margin: 6,
   },
@@ -21,15 +21,9 @@ const styles = StyleSheet.create({
     marginTop: 16,
     marginBottom: 16,
   },
-  images: {
+  image: {
     width: 100,
     height: 100,
-    borderWidth: 2,
-    borderColor: black,
-    borderTopRightRadius: 50,
-    borderBottomLeftRadius: 50,
-    borderBottomRightRadius: 50,
-    borderTopLeftRadius: 50,
   },
   name: {
     marginTop: 8,
@@ -46,6 +40,23 @@ const styles = StyleSheet.create({
     height: 30,
     marginRight: 8,
   },
+  callButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: purple,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: white,
+  },
+  callButtonText: {
+    color: white,
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
 });
 
 export default class ContactDetailContainer extends Component {
@@ -59,26 +70,35 @@ export default class ContactDetailContainer extends Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image style={styles.images} source={require('../../assets/contact.png')} />
-          <Text style={styles.name}>{this.state.contact}</Text>
-        </View>
-        <View>
-          <View style={styles.info}>
-            <Image style={styles.icon} source={require('../../assets/phone.png')} />
-            <Text>XX.XX.XX.XX.XX</Text>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../../assets/contact.png')} />
+            <Text style={styles.name}>{this.state.contact}</Text>
           </View>
-          <View style={styles.info}>
-            <Image style={styles.icon} source={require('../../assets/email.png')} />
-            <Text>nom.prenom@gmail.com</Text>
+          <View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/phone.png')} />
+              <Text>XX.XX.XX.XX.XX</Text>
+            </View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/email.png')} />
+              <Text>nom.prenom@gmail.com</Text>
+            </View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/profile.png')} />
+              <Text>profil</Text>
+            </View>
           </View>
-          <View style={styles.info}>
-            <Image style={styles.icon} source={require('../../assets/profile.png')} />
-            <Text>profil</Text>
-          </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+        <TouchableOpacity
+          style={styles.callButton}
+          onPress={() => Alert.alert('Appel en cours...')}
+          underlayColor="#fff"
+        >
+          <Text style={styles.callButtonText}>Appeler</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
