43ddbc59ef9aed315e2d9a87a14a97445249e5dc
Kevyn <kevyn.buchet@gmail.com>
2019-01-16 12:41:01 +0100

    Modification de l'interface des détails de contact

--- src/components/ContactDetailContainer.js
+++ src/components/ContactDetailContainer.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import {
+  ScrollView, View, Text, Image, StyleSheet,
+} from 'react-native';
+
+const grey = '#F5FCFF';
+const black = '#000';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: grey,
+    margin: 6,
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  images: {
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    borderColor: black,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    borderTopLeftRadius: 50,
+  },
+  name: {
+    marginTop: 8,
+    fontSize: 40,
+  },
+  info: {
+    marginLeft: 24,
+    marginBottom: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginRight: 8,
+  },
+});
 
 export default class ContactDetailContainer extends Component {
   constructor(props) {
@@ -13,9 +59,26 @@ export default class ContactDetailContainer extends Component {
 
   render() {
     return (
-      <View>
-        <Text>{this.state.contact}</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.images} source={require('../../assets/contact.png')} />
+          <Text style={styles.name}>{this.state.contact}</Text>
+        </View>
+        <View>
+          <View style={styles.info}>
+            <Image style={styles.icon} source={require('../../assets/phone.png')} />
+            <Text>XX.XX.XX.XX.XX</Text>
+          </View>
+          <View style={styles.info}>
+            <Image style={styles.icon} source={require('../../assets/email.png')} />
+            <Text>nom.prenom@gmail.com</Text>
+          </View>
+          <View style={styles.info}>
+            <Image style={styles.icon} source={require('../../assets/profile.png')} />
+            <Text>profil</Text>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
