97dcf0b58c9059f668c846b9729b7d8cb5b646a4
Kevyn <kevyn.buchet@gmail.com>
2019-01-17 13:01:35 +0100

    Ajout des textinput pour la modification de contact

--- src/components/ContactDetailContainer.js
+++ src/components/ContactDetailContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  ScrollView, View, Text, Image, StyleSheet, Alert, TouchableOpacity,
+  ScrollView, View, Text, Image, StyleSheet, Alert, TouchableOpacity, TextInput,
 } from 'react-native';
 
 const grey = '#F5FCFF';
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
   image: {
     width: 100,
     height: 100,
+    borderRadius: 50,
   },
   name: {
     marginTop: 8,
@@ -64,33 +65,35 @@ export default class ContactDetailContainer extends Component {
     super(props);
 
     this.state = {
-      contact: this.props.contact,
+      phone: this.props.contact.phone,
+      firstName: this.props.contact.firstName,
+      lastName: this.props.contact.lastName,
+      email: this.props.contact.email,
+      profile: this.props.contact.profile,
+      isFamilinkUser: this.props.contact.isFamilinkUser,
+      isEmergencyUser: this.props.contact.isEmergencyUser,
+      gravatar: this.props.contact.gravatar,
+      edit: this.props.edit,
     };
+
+    this.callOrEditButton = this.callOrEditButton.bind(this);
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          <View style={styles.imageContainer}>
-            <Image style={styles.image} source={require('../../assets/contact.png')} />
-            <Text style={styles.name}>{`${this.state.contact.firstName} ${this.state.contact.lastName}`}</Text>
-          </View>
-          <View>
-            <View style={styles.info}>
-              <Image style={styles.icon} source={require('../../assets/phone.png')} />
-              <Text>{this.state.contact.phone}</Text>
-            </View>
-            <View style={styles.info}>
-              <Image style={styles.icon} source={require('../../assets/email.png')} />
-              <Text>{this.state.contact.email}</Text>
-            </View>
-            <View style={styles.info}>
-              <Image style={styles.icon} source={require('../../assets/profile.png')} />
-              <Text>{this.state.contact.profile}</Text>
-            </View>
-          </View>
-        </ScrollView>
+  handlePhoneChange(text) {
+    this.setState({
+      phone: text,
+    });
+  }
+
+  handleEmailChange(text) {
+    this.setState({
+      email: text,
+    });
+  }
+
+  callOrEditButton() {
+    if (!this.props.edit) {
+      return (
         <TouchableOpacity
           style={styles.callButton}
           onPress={() => Alert.alert('Appel en cours...')}
@@ -98,6 +101,55 @@ export default class ContactDetailContainer extends Component {
         >
           <Text style={styles.callButtonText}>Appeler</Text>
         </TouchableOpacity>
+      );
+    }
+    return (
+      <TouchableOpacity
+        style={styles.callButton}
+        onPress={() => Alert.alert('Contact modifié')}
+        underlayColor="#fff"
+      >
+        <Text style={styles.callButtonText}>Enregister</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.imageContainer}>
+            {this.state.gravatar !== null
+              ? <Image style={styles.image} source={{ uri: this.state.gravatar }} />
+              : <Image style={styles.image} source={require('../../assets/contact.png')} />
+            }
+            <Text style={styles.name}>{`${this.state.firstName} ${this.state.lastName}`}</Text>
+          </View>
+          <View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/phone.png')} />
+              {this.props.edit
+                ? <TextInput value={this.state.phone} onChangeText={text => this.handlePhoneChange(text)} />
+                : <Text>{this.state.phone}</Text>
+              }
+            </View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/email.png')} />
+              {this.props.edit
+                ? <TextInput value={this.state.email} onChangeText={text => this.handleEmailChange(text)} />
+                : <Text>{this.state.email}</Text>
+              }
+            </View>
+            <View style={styles.info}>
+              <Image style={styles.icon} source={require('../../assets/profile.png')} />
+              {this.props.edit
+                ? <TextInput value={this.state.profile} />
+                : <Text>{this.state.profile}</Text>
+              }
+            </View>
+          </View>
+        </ScrollView>
+        {this.callOrEditButton()}
       </View>
     );
   }
@@ -114,4 +166,5 @@ ContactDetailContainer.propTypes = {
     isEmergencyUser: PropTypes.bool.isRequired,
     gravatar: PropTypes.string.isRequired,
   }).isRequired,
+  edit: PropTypes.bool.isRequired,
 };
--- src/screens/ContactDetailScreen.js
+++ src/screens/ContactDetailScreen.js
@@ -15,26 +15,48 @@ const styles = StyleSheet.create({
 });
 
 export default class ContactDetailScreen extends Component {
-  static navigationOptions = () => ({
-    headerRight: (
-      <TouchableHighlight onPress={() => Alert.alert('Supprimer')}>
-        <Image style={styles.icon} source={require('../../assets/delete.png')} />
-      </TouchableHighlight>
-    ),
-  });
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      headerRight: (
+        <>
+          <TouchableHighlight onPress={() => { params.handleThis(); }}>
+            <Image style={styles.icon} source={require('../../assets/edit.png')} />
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => Alert.alert('Supprimer')}>
+            <Image style={styles.icon} source={require('../../assets/delete.png')} />
+          </TouchableHighlight>
+        </>
+      ),
+    };
+  };
 
   constructor(props) {
     super(props);
 
     this.state = {
       contact: this.props.navigation.getParam('contact', 'NO-CONTACT'),
+      edit: false,
     };
+
+    this.changeEditStatus = this.changeEditStatus.bind(this);
   }
 
+  componentDidMount() {
+    this.props.navigation.setParams({
+      handleThis: this.changeEditStatus,
+    });
+  }
+
+  changeEditStatus() {
+    this.setState({
+      edit: !this.state.edit,
+    });
+  }
 
   render() {
     return (
-      <ContactDetailContainer contact={this.state.contact} />
+      <ContactDetailContainer edit={this.state.edit} contact={this.state.contact} />
     );
   }
 }
@@ -42,5 +64,6 @@ export default class ContactDetailScreen extends Component {
 ContactDetailScreen.propTypes = {
   navigation: PropTypes.shape({
     getParam: PropTypes.func.isRequired,
+    setParams: PropTypes.func.isRequired,
   }).isRequired,
 };
