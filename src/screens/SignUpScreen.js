d237108e32d233f478cfe929befe281b75986ee6
Amandine <amandine.richaudeau@outlook.fr>
2019-01-17 15:36:37 +0100

    UI de SignUpScreen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
 export const SIGN_UP_SCENE_NAME = 'SIGN_UP_SCENE';
 
@@ -24,6 +24,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingLeft: 10,
     paddingRight: 10,
+  },
+  textTitle: {
+    color: blue,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    fontSize: 12,
   }
 });
 
@@ -56,6 +64,42 @@ export default class SignUpScreen extends Component {
        render() {
         return (
             <View>
+                <Text style={styles.textTitle}>NUMERO DE TELEPHONE</Text>
+                <TextInput
+                    value={this.state.phone}
+                    onChangeText={(phone) => this.setState({phone})}
+                    placeholder={'Saisissez un numéro'}
+                />
+                <Text style={styles.textTitle}>PRENOM</Text>
+                <TextInput
+                    value={this.state.firstname}
+                    onChangeText={(firstname) => this.setState({firstname})}
+                    placeholder={'Saisissez un prénom'}
+                />
+                <Text style={styles.textTitle}>NOM</Text>
+                <TextInput
+                    value={this.state.lastname}
+                    onChangeText={(lastname) => this.setState({lastname})}
+                    placeholder={'Saisissez un nom de famille'}
+                />
+                <Text style={styles.textTitle}>MOT DE PASSE</Text>
+                <TextInput
+                    value={this.state.password}
+                    onChangeText={(password) => this.setState({password})}
+                    placeholder={'Saisissez un mot de passe'}
+                />
+                <Text style={styles.textTitle}>PROFIL</Text>
+                <TextInput
+                    value={this.state.profileType}
+                    onChangeText={(profile) => this.setState({profile})}
+                    placeholder={'Profil'}
+                />
+                <Text style={styles.textTitle}>E MAIL</Text>
+                <TextInput
+                    value={this.state.email}
+                    onChangeText={(email) => this.setState({email})}
+                    placeholder={'Saisissez un email'}
+                />
                 <TouchableOpacity
                     style={styles.button}
                     onPress={this.navigateToContactsList}
